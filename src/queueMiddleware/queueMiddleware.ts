7f665d10f564d8aac4e864d2f8c125352aba0902
Pavel Tarasov <p.tarasov@alef.im>
2024-11-29 00:35:12 +0300

    Add markdown fallback for sending and editing messages in queue middleware

--- src/queueMiddleware/queueMiddleware.ts
+++ src/queueMiddleware/queueMiddleware.ts
@@ -10,6 +10,36 @@ const requestQueue = new PQueue({
   intervalCap: MAX_REQUESTS_PER_MINUTE,
 });
 
+async function sendMessageWithMarkdownFallback(
+  chatId: number,
+  text: string,
+): Promise<TelegramBot.Message> {
+  try {
+    return await bot.sendMessage(chatId, text, { parse_mode: "Markdown" });
+  } catch (markdownError) {
+    return await bot.sendMessage(chatId, text);
+  }
+}
+
+async function editMessageWithMarkdownFallback(
+  chatId: number,
+  messageId: number,
+  text: string,
+): Promise<TelegramBot.Message | boolean> {
+  try {
+    return await bot.editMessageText(text, {
+      chat_id: chatId,
+      message_id: messageId,
+      parse_mode: "Markdown",
+    });
+  } catch (markdownError) {
+    return await bot.editMessageText(text, {
+      chat_id: chatId,
+      message_id: messageId,
+    });
+  }
+}
+
 export const queueMiddleware = (
   handler: (ctx: TelegramBot.Message) => Promise<string>,
 ) => {
@@ -22,34 +52,98 @@ export const queueMiddleware = (
         const responseText = await handler(ctx);
 
         if (sentMessage) {
-          await bot.editMessageText(responseText, {
-            chat_id: chatId,
-            message_id: sentMessage.message_id,
-          });
+          await editMessageWithMarkdownFallback(
+            chatId,
+            sentMessage.message_id,
+            responseText,
+          );
         } else {
-          await bot.sendMessage(chatId, responseText);
+          await sendMessageWithMarkdownFallback(chatId, responseText);
         }
       } catch (error) {
         console.log(error);
         if (sentMessage) {
-          await bot.editMessageText("Something went wrong...5124", {
-            chat_id: chatId,
-            message_id: sentMessage.message_id,
-          });
+          await editMessageWithMarkdownFallback(
+            chatId,
+            sentMessage.message_id,
+            "Something went wrong...5124",
+          );
         } else {
-          await bot.sendMessage(chatId, "Something went wrong...5111");
+          await sendMessageWithMarkdownFallback(
+            chatId,
+            "Something went wrong...5111",
+          );
         }
       }
     });
 
     if (requestQueue.size > 0) {
-      sentMessage = await bot.sendMessage(
+      sentMessage = await sendMessageWithMarkdownFallback(
         chatId,
         "Your request is in a queue, please wait...",
       );
     }
   };
 };
+
+// import PQueue from "p-queue";
+// import type TelegramBot from "node-telegram-bot-api";
+// import { bot } from "../bot/bot";
+
+// const MAX_REQUESTS_PER_MINUTE = 15;
+
+// const requestQueue = new PQueue({
+//   concurrency: MAX_REQUESTS_PER_MINUTE,
+//   interval: 60000,
+//   intervalCap: MAX_REQUESTS_PER_MINUTE,
+// });
+
+// export const queueMiddleware = (
+//   handler: (ctx: TelegramBot.Message) => Promise<string>,
+// ) => {
+//   return async (ctx: TelegramBot.Message) => {
+//     const chatId = ctx.chat.id;
+//     let sentMessage: TelegramBot.Message | undefined;
+
+//     requestQueue.add(async () => {
+//       try {
+//         const responseText = await handler(ctx);
+
+//         if (sentMessage) {
+//           await bot.editMessageText(responseText, {
+//             chat_id: chatId,
+//             message_id: sentMessage.message_id,
+//           });
+//         } else {
+//           try {
+//             await bot.sendMessage(chatId, responseText, {
+//               parse_mode: "Markdown",
+//             });
+//           } catch (markdownError) {
+//             await bot.sendMessage(chatId, responseText);
+//           }
+//         }
+//       } catch (error) {
+//         console.log(error);
+//         if (sentMessage) {
+//           await bot.editMessageText("Something went wrong...5124", {
+//             chat_id: chatId,
+//             message_id: sentMessage.message_id,
+//           });
+//         } else {
+//           await bot.sendMessage(chatId, "Something went wrong...5111");
+//         }
+//       }
+//     });
+
+//     if (requestQueue.size > 0) {
+//       sentMessage = await bot.sendMessage(
+//         chatId,
+//         "Your request is in a queue, please wait...",
+//       );
+//     }
+//   };
+// };
 
 // export const queueMediaMiddleware = (
 //   handler: (
