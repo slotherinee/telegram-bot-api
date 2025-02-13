d7d6ba5a8fa0a7698a8e4273d9f49662019f1fea
Pavel Tarasov <p.tarasov@alef.im>
2025-02-13 18:04:28 +0300

    обновление версии

--- src/gemini/gemini.ts
+++ src/gemini/gemini.ts
@@ -2,9 +2,9 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const genAI = new GoogleGenerativeAI(Bun.env.GEMINI_TOKEN as string);
 const model = genAI.getGenerativeModel({
-  model: "gemini-1.5-flash",
+  model: "gemini-2.0-flash", 
   systemInstruction:
-    "Write answers as short as possible! Also answer user the same language he asked you. Also do not use your markdown style, you are telegram bot, so use its markdown style: *bold*, _italic_, [inline url](https), `preformatted fixed-with codeblock`, ```block of code```, nothing else",
+    "Also answer user the same language he asked you. Also do not use your markdown style, use this markdown style: *bold*, _italic_, [inline url](https), `preformatted fixed-with codeblock`, ```block of code``` !",
 });
 
 export default model;
