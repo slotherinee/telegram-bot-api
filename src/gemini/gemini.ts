0b2ceac89fa5b0fe56f050bc5759cf6790b9525b
Pavel Tarasov <p.tarasov@alef.im>
2025-02-14 00:20:13 +0300

    t

--- src/gemini/gemini.ts
+++ src/gemini/gemini.ts
@@ -1,22 +1,10 @@
-import { DynamicRetrievalMode, GoogleGenerativeAI } from "@google/generative-ai";
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const genAI = new GoogleGenerativeAI(Bun.env.GEMINI_TOKEN as string);
 const model = genAI.getGenerativeModel({
-  model: "gemini-2.0-flash", 
+  model: "gemini-2.0-flash-lite-preview-02-05", 
   systemInstruction:
-    "Write short or medium-short answers. Also answer user the same language he asked you. Also do not use your markdown style, use this markdown style: *bold*, _italic_, [inline url](https), `preformatted fixed-with codeblock`, ```block of code``` !",
-     tools: [
-      {
-        googleSearchRetrieval: {
-          dynamicRetrievalConfig: {
-            mode: DynamicRetrievalMode.MODE_DYNAMIC,
-            dynamicThreshold: 0.7,
-          },
-        },
-      },
-    ],
-}, {
-  apiVersion: "v1beta"
+    "Write short answers. Speak user the same language he speaks to you. Also do not use your markdown style, use this markdown style: *bold*, _italic_, [inline url](https), `preformatted fixed-with codeblock`, ```block of code``` !",
 });
 
 export default model;
